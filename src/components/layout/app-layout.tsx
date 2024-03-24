3a56564761c8018a45467331fab039df6f8acf9e
ebonian <tanadon.santisan@gmail.com>
2024-03-25 05:24:00 +0700

    feat: mobile only layout

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -1,11 +1,10 @@
+import Header from "../common/header";
 import CustomBackground from "./custom-background";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
-  const backgroundNumber = Math.floor(Math.random() * 3) + 1;
-
   return (
     <div className="flex h-svh w-full justify-center">
-      <div className="h-full w-full max-w-2xl">
+      <div className="h-full w-full max-w-2xl lg:hidden">
         <div className="relative h-full w-full">
           <div className="absolute z-10 h-full w-full overflow-y-auto px-6">
             {children}
@@ -14,6 +13,14 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
             <CustomBackground />
           </div>
         </div>
+      </div>
+      <div className="hidden flex-col items-center justify-center space-y-10 lg:flex">
+        <div className="scale-[2]">
+          <Header />
+        </div>
+        <p className="text-xl font-bold">
+          กรุณาเข้าใช้งานผ่านมือถือหรือแท็บเล็ตเท่านั้นนะ
+        </p>
       </div>
     </div>
   );
