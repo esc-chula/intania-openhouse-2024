2536a8416f2022c22fe45d4d7fd20e93b9dbde9a
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-03-24 21:34:10 +0700

    feat: fetch workshop and tour data for reserve

--- src/app/workshop/reserve/page.tsx
+++ src/app/workshop/reserve/page.tsx
@@ -1,9 +1,50 @@
 "use client";
 
+import { Tour } from "@/common/types/tour";
+import { Workshop } from "@/common/types/workshop";
 import Button from "@/components/ui/button";
 import { Select } from "@/components/ui/select";
+import { useEffect, useState } from "react";
 
 export default function ReserveWorkshop() {
+  const userId = localStorage.getItem("formData") ?? {};
+  const [workshops, setWorkshops] = useState<Workshop[]>([]);
+  const [tours, setTours] = useState<Tour[]>([]);
+
+  useEffect(() => {
+    const fetchWorkshops = async () => {
+      try {
+        const workshopResponse = await fetch("/api/workshop", {
+          method: "GET",
+        });
+        if (!workshopResponse) {
+          throw new Error("Cannot fetch workshops");
+        }
+
+        setWorkshops(await workshopResponse.json());
+      } catch (error) {
+        console.error("Error fetching workshops:", error);
+      }
+    };
+    const fetchTours = async () => {
+      try {
+        const tourResponse = await fetch("/api/tour", {
+          method: "GET",
+        });
+        if (!tourResponse) {
+          throw new Error("Cannot fetch tours");
+        }
+
+        setTours(await tourResponse.json());
+      } catch (error) {
+        console.error("Error fetching tours:", error);
+      }
+    };
+
+    fetchWorkshops();
+    fetchTours();
+  }, []);
+
   const departments = [
     {
       value: "mock1",
@@ -36,6 +77,23 @@ export default function ReserveWorkshop() {
       label: "Mock Time",
     },
   ];
+
+  const handleSubmit = () => {
+    fetch("/api/workshop/reserve", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userId: mobileNumber, workshopId: workshopId }),
+    });
+    fetch("/api/tour/reserve", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userId: mobileNumber, tourId: tourId }),
+    });
+  };
 
   return (
     <div className="flex h-full w-full flex-col justify-between pb-14 pt-4">
@@ -88,6 +146,39 @@ export default function ReserveWorkshop() {
           ))}
         </Select>
       </div>
+
+      <div className="mt-5 flex w-full flex-col items-center space-y-6 ">
+        <h1 className="text-center text-3xl font-bold">Intania Tour</h1>
+        <Select name="date" value="" onChange={() => {}}>
+          <option value="" disabled hidden>
+            วันที่
+          </option>
+          {dates.map((department) => (
+            <option
+              key={department.value}
+              value={department.value}
+              className="text-black"
+            >
+              {department.label}
+            </option>
+          ))}
+        </Select>
+        <Select name="time" value="" onChange={() => {}}>
+          <option value="" disabled hidden>
+            เวลา
+          </option>
+          {times.map((department) => (
+            <option
+              key={department.value}
+              value={department.value}
+              className="text-black"
+            >
+              {department.label}
+            </option>
+          ))}
+        </Select>
+      </div>
+
       <div className="flex flex-col items-center space-y-4">
         <p className="text-center text-xs">
           หากจำนวนคนเต็มแล้ว Workshop
@@ -96,7 +187,7 @@ export default function ReserveWorkshop() {
           <br />
           จะไม่แสดงให้สามารถเลือกได้
         </p>
-        <Button size="default" className="w-36">
+        <Button size="default" className="w-36" onClick={handleSubmit}>
           จอง
         </Button>
       </div>
