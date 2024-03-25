7e225b244354e77e380be9735fb27c9f4af8a99f
ebonian <tanadon.santisan@gmail.com>
2024-03-25 22:52:22 +0700

    fix: register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -93,7 +93,7 @@ export default function Register() {
       <Header />
       <form
         onSubmit={handleSubmit}
-        className="mt-4 flex w-full max-w-md flex-col items-center justify-center gap-5"
+        className="mt-4 flex w-full flex-col items-center justify-center gap-5"
       >
         <Select
           name="prefix"
